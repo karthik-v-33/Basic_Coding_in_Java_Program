8b3cb122c2b538093c70237f2afd962627945f87
Karthik <105358710+karthik-v-33@users.noreply.github.com>
2022-07-07 23:14:59 +0530

    Add files via upload

---
+++ Basicline.java
@@ -0,0 +1,18 @@
+package com.GitHub_Updates;
+/*
+ * class and main method is basic syntax for java program.
+ * class name and file same should be same,
+ * though it can be different but while compiling use file name and while executing use class name.
+ * class name always starts with capital letter.
+ */
+//class is the most basic fundamental unit of writing the java program.
+//class is a collection of variables and methods.
+public class Basicline {
+	//main method is the entry point for the execution inside java program.
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Java Program");
+		System.out.println(2+6);
+	}
+
+}
